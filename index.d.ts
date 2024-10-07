b740ec9f1b617644d32bc2f52d891890e1eed364
Johann Schopplich <mail@johannschopplich.com>
2024-10-07 15:30:05 +0200

    feat: explicit named exports

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
-export * from "./src/api";
-export * from "./src/blocks";
-export * from "./src/kql";
-export * from "./src/layout";
-export * from "./src/query";
+export { KirbyApiResponse } from "./src/api";
+export {
+  KirbyBlock,
+  KirbyDefaultBlocks,
+  KirbyDefaultBlockType,
+} from "./src/blocks";
+export {
+  KirbyQueryRequest,
+  KirbyQueryResponse,
+  KirbyQuerySchema,
+} from "./src/kql";
+export { KirbyLayout, KirbyLayoutColumn } from "./src/layout";
+export { KirbyQuery, KirbyQueryChain, KirbyQueryModel } from "./src/query";
