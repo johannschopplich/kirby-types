b324a696f4b3751ca5ef0aa983f3fd719298e107
Johann Schopplich <mail@johannschopplich.com>
2024-10-08 07:07:03 +0200

    fix: explicitly use `export type` syntax

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-export { KirbyApiResponse } from "./src/api";
-export {
+export type { KirbyApiResponse } from "./src/api";
+export type {
   KirbyBlock,
   KirbyDefaultBlocks,
   KirbyDefaultBlockType,
 } from "./src/blocks";
-export {
+export type {
   KirbyQueryRequest,
   KirbyQueryResponse,
   KirbyQuerySchema,
 } from "./src/kql";
-export { KirbyLayout, KirbyLayoutColumn } from "./src/layout";
-export { KirbyQuery, KirbyQueryChain, KirbyQueryModel } from "./src/query";
+export type { KirbyLayout, KirbyLayoutColumn } from "./src/layout";
+export type { KirbyQuery, KirbyQueryChain, KirbyQueryModel } from "./src/query";
